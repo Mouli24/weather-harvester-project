93bd81158b1e64eb9717a23a0591626e0b6c9b39
Aastha-spec-tech <explorethings12345@gmail.com>
2025-12-06 04:53:43 +0530

    added code for turtlr alert

--- weather_harvester/turtle_alert.py
+++ weather_harvester/turtle_alert.py
@@ -7,18 +7,13 @@ def draw_clean_skull(t):
     t.color("white")
     t.pensize(4)
 
-    # --------------------
-    # Skull Head (clean)
-    # --------------------
     t.penup()
     t.goto(0, 30)
     t.setheading(0)
     t.pendown()
     t.circle(60)  # smoother round skull
 
-    # --------------------
-    # Jaw (rectangle)
-    # --------------------
+    
     t.penup()
     t.goto(-45, 30)
     t.setheading(-90)
@@ -29,9 +24,7 @@ def draw_clean_skull(t):
     t.setheading(90)
     t.forward(70)
 
-    # --------------------
-    # Eyes (simple ovals)
-    # --------------------
+    
     t.penup()
     t.goto(-25, 60)
     t.pendown()
@@ -46,9 +39,7 @@ def draw_clean_skull(t):
     t.circle(15)
     t.end_fill()
 
-    # --------------------
-    # Nose
-    # --------------------
+    
     t.penup()
     t.goto(0, 40)
     t.pendown()
@@ -61,9 +52,7 @@ def draw_clean_skull(t):
     t.forward(15)
     t.end_fill()
 
-    # --------------------
-    # Teeth (clean vertical lines)
-    # --------------------
+    
     t.pensize(3)
     for x in [-20, -10, 0, 10, 20]:
         t.penup()
@@ -72,9 +61,7 @@ def draw_clean_skull(t):
         t.pendown()
         t.forward(40)
 
-    # --------------------
-    # Crossbones
-    # --------------------
+    
     t.pensize(7)
     t.color("white")
 
@@ -90,4 +77,25 @@ def draw_clean_skull(t):
     t.pendown()
     t.forward(180)
 
+def turtle_clean_skull_alert(flashes=10):
+    screen = turtle.Screen()
+    screen.title("DANGER ALERT")
+    screen.bgcolor("black")
 
+    t = turtle.Turtle()
+    draw_clean_skull(t)
+
+    # Title text
+    t.penup()
+    t.goto(0, 120)
+    t.color("red")
+    t.write("DANGER ALERT!", align="center", font=("Arial", 28, "bold"))
+
+    # Flashing background
+    for _ in range(flashes):
+        screen.bgcolor("red")
+        time.sleep(0.15)
+        screen.bgcolor("black")
+        time.sleep(0.15)
+
+    screen.mainloop()
